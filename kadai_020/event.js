4328641c63488117411542609d318001e1cc9130
ogura25 <ogura25@e-corporation.co.jp>
2026-04-16 11:54:35 +0900

    kadai_020
    kadai_020をプッシュ

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const text = document.getElementById("text")
+const button1 = document.getElementById("btn")
+button1.addEventListener('click' , function(){
+    text.textContent ='ボタンをクリックしました'
+})
